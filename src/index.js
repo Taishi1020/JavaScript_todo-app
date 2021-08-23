fd8a75ef96396586004dfc297bbbdfeb49d1ea2b
泰志1020 <70305895+Taishi1020@users.noreply.github.com>
2021-08-23 10:08:47 +0900

    追加ボタンに値を取得 追加ボタンにアラートで入力した内容が表示される機能

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //いきなり機能を実装するのでは無く関数がうまく動作しているかの確認(細分化)
+  const inputText = document.getElementById("add-text").value;
+  alert(inputText);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
